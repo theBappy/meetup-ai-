a0c838b3e099a21b21ac6fe0e1ec0f8e0cee4e78
theBappy <abappy575@gmail.com>
2025-06-02 11:25:33 +0600

    authentication ui setup

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/components/ui/card'
+import { SignInView } from '@/modules/auth/ui/views/sign-in-view';
+
+const page = () => {
+  return (
+        <SignInView />
+  );
+};
+
+export default page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/components/ui/card'
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
+
+const page = () => {
+  return (
+    <SignUpView />
+  )
+}
+
+export default page
