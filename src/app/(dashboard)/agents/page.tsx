7426c4eb0e655f4707e2ae5e58c4ac654ff85456
theBappy <abappy575@gmail.com>
2025-06-05 11:55:12 +0600

    agents filtering and pagination

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -11,8 +11,16 @@ import { AgentListHeader } from "@/modules/agents/server/ui/components/agent-lis
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import type { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/modules/agents/params";
 
-const page = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+
+const page = async ({searchParams}: Props) => {
+  const filters = await loadSearchParams(searchParams)
+
   const session = await auth.api.getSession({
       headers: await headers(),
     });
@@ -22,7 +30,9 @@ const page = async () => {
 
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+    ...filters
+  }));
 
   return (
     <>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from 'nuqs/adapters/next'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <NuqsAdapter>
     <TRPCReactProvider>
       <html lang="en">
         <body className={`${inter.className} antialiased`}>
@@ -27,5 +29,6 @@ export default function RootLayout({
         </body>
       </html>
     </TRPCReactProvider>
+    </NuqsAdapter>
   );
 }
